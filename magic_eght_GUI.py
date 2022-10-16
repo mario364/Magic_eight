2170425edb90d675376e39de4188806d694f8ddb
Maxim Savikin <maxsavikin@gmail.com>
2022-10-16 14:25:58 +0300

    final commit 1.0

--- magic_eght_GUI.py
+++ magic_eght_GUI.py
@@ -1,18 +1,17 @@
 import random
 import PySimpleGUI as sg
-import time
 
-answers = ['Бесспорно', 'Предрешено', 'Определённо да', 'Можешь быть уверен в этом', 'Мне кажется - да', 'Вероятнее '
-                                                                                                         'всего',
-           'Хорошие перспективы', 'Знаки говорят - да', 'Да', 'Пока неясно, попробуй снова', 'Спроси позже',
-           'Лучше не рассказывать', 'Сейчас нельзя предсказать', 'Сконцентрируйся и спроси опять', 'Даже не думай',
-           'Мой ответ - нет', 'По моим данным - нет', 'Перспективы не очень хорошие', 'Весьма сомнительно' ]
+
+answers = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png',
+           '7.png', '8.png', '9.png', '10.png', '11.png',
+           '12.png', '13.png', '14.png', '15.png',
+           '16.png', '17.png', '18.png', '19.png' ]
 
 layout = [
-    [sg.Image('magic_eight_right.png')],
-    [sg.Text('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.Введи свой вопрос')],
+    [sg.Image('Emty_ball.png', key='img')],
+    [sg.Text('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.Введи свой вопрос', key='text')],
     [sg.InputText(key='input'), sg.Button('Спросить')],
-    [sg.Text('', visible=False, key='text')]
+    [sg.Button('Выход')]
 ]
 
 
@@ -26,13 +25,12 @@ while True:
         break
     if event == 'Спросить':
         if values['input']:
-            window['text'].Update(random.choice(answers), visible=True)
-        time.sleep(2)
-        answer = sg.PopupYesNo('Спросить еще?')
-        if answer == 'Yes':
-            continue
-        else:
-            break
+            window['img'].Update(random.choice(answers))
+        if not values['input']:
+            window['text'].Update('Вы ничего не спросили')
+
+    if event == 'Выход':
+        break
 
 
 
